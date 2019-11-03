954f7ccc18566f718f4d7b3066b741db2692cd9b
Gyarmathy Bálint <bulint1@gmail.com>
2019-11-03 22:20:24 +0100

    gyak1

---
+++ gy1/rand.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+int main() {
+  srand(time(NULL));    // the starting value of random number generation
+  int r = rand() % 100; // number between 0-99 //gyerekben pid jó ötlet
+  printf("Random number %i\n", r);
+  return 0;
+}
