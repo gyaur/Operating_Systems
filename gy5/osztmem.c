0c207b5f5c5e2d5239829141f7b06bfa27a5c224
Gyarmathy Bálint <bulint1@gmail.com>
2019-12-11 22:11:46 +0100

    extra stuff added

--- gy5/osztmem.c
+++ gy5/osztmem.c
@@ -12,35 +12,44 @@ int main(int argc, char *argv[]) {
 
   int pid;
   key_t kulcs;
-  int oszt_mem_id;
   char *s;
+  int *sd;
   // a parancs nevevel es az 1 verzio szammal kulcs generalas
-  kulcs = ftok(argv[0], 1);
-  // osztott memoria letrehozasa, irasra olvasasra, 500 bajt mrettel
-  oszt_mem_id = shmget(kulcs, 500, IPC_CREAT | S_IRUSR | S_IWUSR);
+  // osztott memoria letrehozasa, irasra olvasasra, 500 bajt mérettel
+  int oszt_mem_id = shmget(IPC_PRIVATE, 500, IPC_CREAT | S_IRUSR | S_IWUSR);
+  int oszt_mem_id2 = shmget(IPC_PRIVATE, 500, IPC_CREAT | S_IRUSR | S_IWUSR);
+
   // csatlakozunk az osztott memoriahoz,
   // a 2. parameter akkor kell, ha sajat cimhez akarjuk illeszteni
   // a 3. paraméter lehet SHM_RDONLY, ekkor csak read van
-  s = shmat(oszt_mem_id, NULL, 0);
+  sd = shmat(oszt_mem_id, NULL, 0);
+  s = shmat(oszt_mem_id2, NULL, 0);
   //
   pid = fork();
   if (pid > 0) {
 
-    char buffer[] = "Hajra Fradi! \n";
+    char buffer[] = "Hajra Újpest! \n";
     // beirunk a memoriaba
     strcpy(s, buffer);
+    int a = 5;
+    *sd = a;
     // elengedjuk az osztott memoriat
+    shmdt(sd);
     shmdt(s);
+
     //	s[0]='B';  //ez segmentation fault hibat ad
     wait(NULL);
     // IPC_RMID- torolni akarjuk a memoriat, ekkor nem kell 3. parameter
     // IPC_STAT- osztott memoria adatlekerdezes a 3. parameterbe,
     //  ami shmid_ds struct tipusu mutato
     shmctl(oszt_mem_id, IPC_RMID, NULL);
+    shmctl(oszt_mem_id2, IPC_RMID, NULL);
+
   } else if (pid == 0) {
     sleep(1);
-    printf("A gyerek ezt olvasta az osztott memoriabol: %s", s);
+    printf("A gyerek ezt olvasta az osztott memoriabol: %i,%s\n", *sd, s);
     // gyerek is elengedi
+    shmdt(sd);
     shmdt(s);
   }
 
